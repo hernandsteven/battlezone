6dd2e31b4ef8fe0a51626a4bb8328656bb7e2af2
hernandsteven <hernandsteven@gmail.com>
2022-11-17 00:21:05 -0500

    added tournaments to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,21 @@
 'use client'
 import { Auth, ThemeSupa } from '@supabase/auth-ui-react'
 import {
-	Session,
 	useSession,
 	useSupabaseClient,
 	useUser,
 } from '@supabase/auth-helpers-react'
 import useSessionStore from '../stores/sessionStore'
 import { useEffect, useState } from 'react'
+import Tournaments from '../components/Tournaments'
 
 export default function Home() {
 	const supabase = useSupabaseClient()
 	const session = useSession()
-	const user = useUser()
 
 	return (
 		<>
-			<div className="flex bg-primary h-[100vh] w-full p-4">
+			<div className="flex bg-primary w-full p-4 h-screen">
 				{!session && (
 					<div className="flex mx-auto self-center">
 						<Auth
@@ -35,9 +34,8 @@ export default function Home() {
 					</div>
 				)}
 				{session && (
-					<div className="flex mx-auto self-center">
-						<h1 className="text-white text-2xl">{user?.id}</h1>
-						<button onClick={() => {}}>Update me</button>
+					<div className="flex flex-col w-full">
+						<Tournaments tournaments={{}} />
 					</div>
 				)}
 			</div>
