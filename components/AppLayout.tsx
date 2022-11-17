edb4b5e4f3bb774ffd80491cbff16e82748d0eb9
hernandsteven <hernandsteven@gmail.com>
2022-11-17 00:21:48 -0500

    Added welcome to top of layout

--- components/AppLayout.tsx
+++ components/AppLayout.tsx
@@ -1,21 +1,28 @@
-import { Session, useSession } from '@supabase/auth-helpers-react'
+'use client'
+import { useSession, useUser } from '@supabase/auth-helpers-react'
 import React, { useEffect } from 'react'
-import { Provider } from '../app/provider'
-import useSessionStore from '../stores/sessionStore'
 import Filter from './Filter'
 import Sidebar from './Sidebar'
 
 const AppLayout = ({ children }: { children: React.ReactNode }) => {
+	const user = useUser()
+	const session = useSession()
 	return (
-		<>
-			<div className="flex h-full">
-				<Sidebar />
-				<div className="flex flex-col w-full">
-					<Filter />
-					<div className="overflow-y-auto">{children}</div>
-				</div>
+		<div className="flex h-full">
+			<Sidebar />
+			<div className="flex flex-col w-full">
+				{session && (
+					<div className="flex flex-row p-1 gap-1 bg-primary border-b border-tertiary">
+						<h1 className="font-semibold ">Welcome, </h1>
+						<h1 className="font-semibold text-quaternary">
+							{user?.email}
+						</h1>
+					</div>
+				)}
+				<Filter />
+				<div className="overflow-y-auto">{children}</div>
 			</div>
-		</>
+		</div>
 	)
 }
 
